dafaf2b9a943b30b0592d6182d5a89c4cc703111
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-06-22 17:29:05 +0200

    modified default structure. removed things not implemented.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
