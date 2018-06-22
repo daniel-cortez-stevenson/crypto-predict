6c7a51cfaf8c2c3d9df835b3b2d8ed7f322fe415
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-06-22 17:17:46 +0200

    first commit. initialize project structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='A wonderful data science challenge!',
+    author='Daniel C Stevenson',
+    license='MIT',
+)
