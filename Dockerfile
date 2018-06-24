6f69b8b4e446407bc63857f40c6485e9ee46fa67
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-06-24 21:54:40 +0200

    updated how coin param is passed. print times with predictions.

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,15 @@ FROM python:3.5.5-jessie
 
 WORKDIR /app
 
+# Move necessary components over to container
 ADD src /app/src
 ADD setup.py /app/setup.py
 ADD requirements.txt /app/requirements.txt
 
+# Install packages and upgrade Flask to development version
 RUN pip install -U pip setuptools wheel
 RUN pip install -r requirements.txt
+RUN pip install -U https://github.com/pallets/flask/archive/master.tar.gz
 
 ADD models /app/models
 
--- src/app.py
+++ src/app.py
@@ -1,13 +1,11 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, Request
+from flask import Flask, jsonify, abort, request, Response
 from src.data import get_data
 from src.models import load_model
 from src.features import build_features
-import json
+import pandas as pd
 
 app = Flask(__name__)
-
-
 
 
 @app.route('/')
@@ -17,16 +15,50 @@ def index():
 @app.route('/predict', methods=['GET'])
 def get_prediction():
 
-    coin = Request.args.get("coin", "")
+    coin = ''
+    try:
+        coin = request.args.get('coin')
+    except KeyError:
+        # Want to put more descriptive errors here.
+        abort(404)
+    except Exception as e:
+        abort(404)
+        print(e)
+
+
+    # Could alter this to gerneralize the function
     if coin != 'BTC':
         abort(404)
 
-    model = load_model.load_model_from_path('/app/models/linear_model_btc.pkl')
-    data = get_data.retrieve_all_data(coin, 24)
+    # Then you would match coins to a model path using a dict.
+    # Loading this model here rather than at app start because scalability is not an issue (yet!)
+    model = load_model.load_model_from_path('models/linear_model_btc.pkl')
+
+    data = pd.DataFrame()
+    # I would have the app subscribe to the hourly data from cryptocompare and then query the cryptocompare API only hourly
+    try :
+        data = get_data.retrieve_all_data(coin, 24)
+    except:
+        abort(404)
+
     data = build_features.prep_df(data)
+
+    current_time = list(data['time'])[-1]
+    predict_times = [current_time + 3600 * ix for ix, x in enumerate(range(6))]
+    predict_times = pd.to_datetime(predict_times, unit='s')
+
     data = build_features.series_to_supervised(data, n_in=24, n_out=0, dropnan=True)
+
     prediction = model.predict(data)
-    return jsonify({'predictions': prediction[0]})
+
+    # Not a UI guy so here ya go
+    return jsonify({str(predict_times[0]): prediction[0][0],
+                    str(predict_times[1]): prediction[0][1],
+                    str(predict_times[2]): prediction[0][2],
+                    str(predict_times[3]): prediction[0][3],
+                    str(predict_times[4]): prediction[0][4],
+                    str(predict_times[5]): prediction[0][5]
+                    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
