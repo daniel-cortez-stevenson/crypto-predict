562a29dc74eb0fb463de265385b530030e138e80
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-10-15 21:06:42 +0200

    changes to dropout and documentation

--- src/models/zoo.py
+++ src/models/zoo.py
@@ -1,7 +1,7 @@
 """
 Welcome to the Zoo. All the different models - for your viewing!
 """
-from keras.layers import Input, LSTM, BatchNormalization, Dense, Conv1D, TimeDistributed, Activation
+from keras.layers import Input, LSTM, GRU, BatchNormalization, Dense, Conv1D, TimeDistributed, Activation, Dropout
 from keras import Model
 
 
@@ -9,23 +9,29 @@ def LSTM_triggerNG(input_shape, num_outputs, kernel_init='normal', bias_init='ze
     """
     Modified from Andrew Ng's Deeplearning.ai Sequence Modeling course on Coursera.
     Originally used for trigger word detection.
-    Alternative Archetecture w/ Conv1D
+    Notes:
+        - Switched GRU cells to LSTM cells
+        - BiDirectional
+        - Custom, guesswork kernel_size and strides in Conv1D
     """
 
     model_input = Input(shape=input_shape, dtype='float32')
 
-    X = Conv1D(128, kernel_size=4, strides=2, kernel_initializer=kernel_init, bias_initializer=bias_init)(model_input)
+    X = Conv1D(196, kernel_size=4, strides=2, kernel_initializer=kernel_init, bias_initializer=bias_init)(model_input)
     X = BatchNormalization(axis=-1)(X)
     X = Activation('relu')(X)
+    X = Dropout(0.8)(X)
 
-    X = LSTM(units=64, return_sequences=True, kernel_initializer=kernel_init, bias_initializer=bias_init)(X)
+    X = LSTM(units=128, return_sequences=True, go_backwards=True, kernel_initializer=kernel_init, bias_initializer=bias_init)(X)
+    X = Dropout(0.8)(X)
     X = BatchNormalization(axis=-1)(X)
 
-    X = LSTM(units=64, return_sequences=False, kernel_initializer=kernel_init, bias_initializer=bias_init)(X)
+    X = LSTM(units=128, return_sequences=False, go_backwards=True, kernel_initializer=kernel_init, bias_initializer=bias_init)(X)
+    X = Dropout(0.8)(X)
     X = BatchNormalization(axis=-1)(X)
+    X = Dropout(0.8)(X)
 
-    X = Dense(num_outputs, kernel_initializer=kernel_init, bias_initializer=bias_init, activation='linear') \
-        (X) # time distributed  (linear)
+    X = Dense(num_outputs, kernel_initializer=kernel_init, bias_initializer=bias_init, activation='linear')(X)
 
     model = Model(inputs=model_input, outputs=X)
     return model
@@ -37,7 +43,6 @@ def LSTM_WSAEs(input_shape, num_outputs, encoding_dim=10, kernel_init='normal', 
     TODO:
         ?num_delays = 5
         ?stateful
-        ?return_sequences
 
     From Paper:
         "A deep learning framework for financial time series using stacked autoencoders and long-short term memory"
@@ -61,8 +66,7 @@ def LSTM_WSAEs(input_shape, num_outputs, encoding_dim=10, kernel_init='normal', 
     X = LSTM(units=64, return_sequences=False, kernel_initializer=kernel_init, bias_initializer=bias_init)(X)
     X = BatchNormalization(axis=-1)(X)
 
-    X = Dense(num_outputs, kernel_initializer=kernel_init, bias_initializer=bias_init, activation='linear')(
-        X)  # time distributed  (linear)
+    X = Dense(num_outputs, kernel_initializer=kernel_init, bias_initializer=bias_init, activation='linear')(X)
 
     model = Model(inputs=model_input, outputs=X)
     return model
