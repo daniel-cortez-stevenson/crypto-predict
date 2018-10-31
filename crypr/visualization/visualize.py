543af5acb70719b3588063f9522df7e318ed2adf
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-10-31 11:18:10 +0100

    generalized learning_curve fun

--- crypr/visualization/visualize.py
+++ crypr/visualization/visualize.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-def learning_curve(fitted_keras_model):
+def learning_curve(fitted_keras_model, loss_train='loss', loss_val='val_loss'):
     """
     Check out loss from train and dev sets
     """
     plt.figure()
-    plt.plot(fitted_keras_model.history['loss'], label='train')
-    plt.plot(fitted_keras_model.history['val_loss'], label='test')
+    plt.plot(fitted_keras_model.history[loss_train], label='train')
+    plt.plot(fitted_keras_model.history[loss_val], label='val')
     plt.legend()
     plt.show()
