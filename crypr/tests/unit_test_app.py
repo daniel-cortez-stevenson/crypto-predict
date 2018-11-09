537f4098a694ad0e5673fbeaaa7676e2c7145de5
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-11-09 16:06:28 +0100

    URL param contains 0.0.0.0:5000/predict now. Fixes problem with domain being None.
    Fixes:
       https://github.com/daniel-cortez-stevenson/crypto-predict/issues/1
       https://github.com/daniel-cortez-stevenson/crypto-predict/issues/2

--- crypr/tests/unit_test_app.py
+++ crypr/tests/unit_test_app.py
@@ -9,7 +9,6 @@ class BrokenLinkTest(unittest.TestCase):
 
     def setUp(self):
         ''' Define some unique data for validation '''
-        self.domain = "http://0.0.0.0:5000"
         self.acceptable_codes = [200]
 
     def tearDown(self):
@@ -18,7 +17,6 @@ class BrokenLinkTest(unittest.TestCase):
         self.acceptable_codes = None
 
     def request_prediction(self, url=None, coins=None):
-        url = self.domain + url
         results=[]
         for coin in coins:
             r = requests.get('{}?coin={}'.format(url, coin), allow_redirects=False, verify=False)
@@ -29,7 +27,7 @@ class CrawlSite(BrokenLinkTest):
     ''' Verify no broken links are present within blog '''
     def runTest(self):
         ''' Execute recursive request '''
-        results = self.request_prediction("/predict", ['BTC', 'ETH'])
+        results = self.request_prediction("http://0.0.0.0:5000/predict", ['BTC', 'ETH'])
         for r in results:
             status=r[1].status_code
             coin=r[0]
