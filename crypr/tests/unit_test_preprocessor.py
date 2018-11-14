1b8b23822af4945e5b8f60e5bb9c3aa5dfa09b8d
Daniel Cortez Stevenson <daniel.cortez.stevenson@gmail.com>
2018-11-14 02:47:18 +0100

    expand shapeCheck and add reshapeCheck testing funs.

--- crypr/tests/unit_test_preprocessor.py
+++ crypr/tests/unit_test_preprocessor.py
@@ -6,6 +6,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from crypr.base.preprocessors import *
 
+
 class TestPreprocessor(unittest.TestCase):
     ''' Base class for testing preprocessors '''
 
@@ -19,6 +20,7 @@ class TestPreprocessor(unittest.TestCase):
         self.target_col = 'close'
         self.wavelet = 'haar'
         self.moving_averages = [6,12,24,48,72]
+        self.dummy_arr_2d = np.reshape(np.arange(5*4*3), (5, 12))
 
     def tearDown(self):
         ''' Destroy unique data '''
@@ -30,8 +32,16 @@ class TestPreprocessor(unittest.TestCase):
         self.moving_averages = None
 
     def shapeCheck(self, X_processed):
-        data_shape=X_processed.shape
-        self.assertTrue(self.Tx == data_shape[-1])
+        self.assertTrue(self.Tx == X_processed.shape[-1])
+        self.assertTrue(len(X_processed.shape) == 3)
+
+    def reshapeCheck(self, reshaped_data):
+        dummy = self.dummy_arr_2d
+        # Same # of values
+        self.assertEqual(dummy.size, reshaped_data.size)
+        # Equal sum of values
+        self.assertEqual((dummy[0].sum(), dummy[0].mean(), dummy[0].std()),
+                        (reshaped_data[0].sum(), reshaped_data[0].mean(), reshaped_data[0].std()))
 
 
 class TestDWTSmoothPreprocessor(TestPreprocessor):
@@ -46,6 +56,9 @@ class TestDWTSmoothPreprocessor(TestPreprocessor):
         X = preprocessor.fit_transform(self.data)
         self.shapeCheck(X)
 
+        dummy_reshaped = preprocessor._reshape(self.dummy_arr_2d)
+        self.reshapeCheck(dummy_reshaped)
+
 
 class TestSimplePreprocessor(TestPreprocessor):
 
@@ -59,3 +72,7 @@ class TestSimplePreprocessor(TestPreprocessor):
         preprocessor.production = True
         X = preprocessor.fit_transform(self.data)
         self.shapeCheck(X)
+
+        preprocessor.Tx = 3
+        dummy_reshaped = preprocessor._reshape(pd.DataFrame(self.dummy_arr_2d))
+        self.reshapeCheck(dummy_reshaped)
